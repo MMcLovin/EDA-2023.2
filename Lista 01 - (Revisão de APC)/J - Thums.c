c5835faa8c1ba91fabe8ecb013fb41369d0db551
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-09-15 23:39:14 -0300

    Update J - Thums.c

--- Lista 01 - (Revisão de APC)/J - Thums.c
+++ Lista 01 - (Revisão de APC)/J - Thums.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
 int main() {
+     int N;
+
+    scanf("%d", &N);
     
-    
+     for (int i = 1; i <= N; i++)
+    {
+        if (i % 2 == 0)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                printf("THUMS ");
+            }
+        }
+        else
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                printf("THUMS ");
+            }
+        }       
+        printf("\n");
+    } 
     return 0;
 }
