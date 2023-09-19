a612f362667938866bc52935749c3c588a095bd8
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-09-18 21:54:58 -0300

    Update M - Menor índice do vetor.c

--- Lista 01 - (Revisão de APC)/M - Menor índice do vetor.c
+++ Lista 01 - (Revisão de APC)/M - Menor índice do vetor.c
@@ -2,6 +2,30 @@
 
 int main() {
     
+    int vetSize;
+
+    scanf("%d", &vetSize);
+
+    int vet[vetSize];
+
+    for(int i = 0; i < vetSize; i++)
+    {
+        int input;
+        scanf("%d", &input);
+        vet[i] = input;
+    }
+
+    int indexMenor = 0;
+
+    for (int i = 0; i < vetSize; i++)
+    {
+        if (vet[indexMenor] > vet[i])
+        {
+            indexMenor = i;
+        }
+    }
+
+    printf("%d\n", indexMenor);
     
     return 0;
 }
