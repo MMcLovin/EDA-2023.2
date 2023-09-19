1c5e792da611cb3ac13023a933589884b5670740
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-09-18 23:23:13 -0300

    Update O - Pares e ímpares do vetor (vetor).c

--- Lista 01 - (Revisão de APC)/O - Pares e ímpares do vetor (vetor).c
+++ Lista 01 - (Revisão de APC)/O - Pares e ímpares do vetor (vetor).c
@@ -2,6 +2,55 @@
 
 int main() {
     
+    int vetSize;
+
+    scanf("%d", &vetSize);
+
+    int vet[vetSize];
+    int vetPar[vetSize];
+    int vetImpar[vetSize];
+
+    for(int i = 0; i < vetSize; i++)
+    //le os n elementos do vetor
+    {
+        int input;
+        scanf("%d", &input);
+        vet[i] = input;
+    }
+
+    int j = 0, k = 0;
     
+    for (int i = 0; i < vetSize; i++)
+    //varre vetor
+    {
+        if (vet[i] % 2 == 0)
+        {
+            //eh par
+            vetPar[j] = vet[i];
+            j++;
+        }
+        else
+        {
+            //eh impar
+            vetImpar[k] = vet[i];
+            k++;
+        }
+        
+    }
+
+    //imprime par
+    for (int i = 0; i < j; i++)
+    {
+        printf("%d ", vetPar[i]);
+    }
+    
+    printf("\n");
+    
+    //imprime impar
+    for (int i = 0; i < k; i++)
+    {
+        printf("%d ", vetImpar[i]);
+    }
+      
     return 0;
 }
