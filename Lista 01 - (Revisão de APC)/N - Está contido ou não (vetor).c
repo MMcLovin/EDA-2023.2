f4c860af7928b56768686875ce5dfa1434949490
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-09-18 23:23:17 -0300

    Update N - Está contido ou não (vetor).c

--- Lista 01 - (Revisão de APC)/N - Está contido ou não (vetor).c
+++ Lista 01 - (Revisão de APC)/N - Está contido ou não (vetor).c
@@ -2,6 +2,42 @@
 
 int main() {
     
+    int vetSize;
+
+    scanf("%d", &vetSize);
+
+    int vet[vetSize];
+
+    for(int i = 0; i < vetSize; i++)
+    //le os n elementos do vetor
+    {
+        int input;
+        scanf("%d", &input);
+        vet[i] = input;
+    }
+
+    int toFind;
+    int result = 0;//0 = not in the vet. 1 = inside the vet
+
+    //le o elemento a ser procurado no vetor
+    scanf("%d", &toFind);
+
+    for (int i = 0; i < vetSize; i++)
+    {
+        if (vet[i] == toFind)
+        {
+            result = 1;
+        }
+    }
+
+    if (result == 1)
+    {
+        printf("pertence\n");
+    }
+    else
+    {
+        printf("nao pertence\n");
+    }
     
     return 0;
 }
