fae965aad4a3a3747bfedc33ee7584f3ad4d1c23
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-09-20 11:56:01 -0300

    Update R - Chuva (matriz).c

--- Lista 01 - (Revisão de APC)/R - Chuva (matriz).c
+++ Lista 01 - (Revisão de APC)/R - Chuva (matriz).c
@@ -1,57 +1,64 @@
 #include <stdio.h>
 
-void inicializaVetor(int *vet, int linhas, int colunas)
-{
-    int *vetP = vet;
-
-    for (int i = 0; i < linhas; i++)
-    {
-        for (int j = 0; j < colunas; j++)
-        {
-            vetP[i][j] = 0;
-        }
-    }
-}
 
 void imprimeMatriz(int *vet, int linhas, int colunas)
 {
-    int *vetP = vet;
-
     for (int i = 0; i < linhas; i++)
     {
         for (int j = 0; j < colunas; j++)
         {
-            printf("%d ", vetP[i][j]);
+            printf("%d ", *(vet + (i*colunas) + j));
         }
         printf("\n");
     }
 }
 
-int main() {
+void somaMatriz(int *resultado, int *M1, int *M2, int linhas, int colunas)
+{
+    for (int i = 0; i < linhas; i++)
+    {
+        for (int j = 0; j < colunas; j++)
+        {
+            *(resultado + i*colunas + j) = *(M1 + i*colunas + j) + *(M2 + i*colunas + j);
+        }
+    }
+}
+
+int main() 
+{
     int dimensao;
     
     scanf("%d", &dimensao);
 
-    int linhas = dimensao * 2;
-    int mapa[linhas][dimensao];//cada elemento do mapa é uma 
-    //região
+    int linhasEntrada = dimensao * 2;
+    int mapaP1[dimensao][dimensao];
+    int mapaP2[dimensao][dimensao];
+    //numero de regioes = NxN
+    //as dimensao*2 são dois mapas, cada um de um periodo distinto
+    //é so uma soma de matrizes pqp, passei 3h pra entender isso
+    //a cada dimensao * 2 linhas, nos temos um mapa
 
-    printf("vetor com lixo de memoria\n");
-    imprimeMatriz(mapa, linhas, dimensao);
-
-/*     for (int i = 0; i < linhas; i++)
-    //for que vai percorrer as 2*dimensão
-    //só q cada linha precisa de um outro for para 
-    //ler n mapas
+    for (int i = 0; i < dimensao; i++)
+    //for que vai percorrer as 2*dimensão para o mapa do primeiro periodo, onde temos que ler as duas matrizes que devem ser somadas
     {
-        for (int i = 0; i < dimensao; i++)
+        for (int j = 0; j < dimensao; j++)
         {
-            scanf("%d", &mapa);
+            scanf("%d", &mapaP1[i][j]);
         }
-    } */
-
-    printf("vetor inicializado por uma função\n");
-    imprimeMatriz(mapa, linhas, dimensao);
+    }
+    for (int i = 0; i < dimensao; i++)
+    //mapa do segundo periodo
+    {
+        for (int j = 0; j < dimensao; j++)
+        {
+            scanf("%d", &mapaP2[i][j]);
+        }
+    }
+        
+    int resultado[dimensao][dimensao];
+    
+    somaMatriz((int *)resultado, (int *)mapaP1, (int *)mapaP2, dimensao, dimensao);
+    imprimeMatriz((int *)resultado, dimensao, dimensao);
 
     return 0;
 }
