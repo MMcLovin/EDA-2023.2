01223283e06f88ecfc9fede03b37a1dc30ed8686
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-09-21 00:00:07 -0300

    Update P - Índices elementos pares e ímpares do vetor (vetor).c

--- Lista 01 - (Revisão de APC)/P - Índices elementos pares e ímpares do vetor (vetor).c
+++ Lista 01 - (Revisão de APC)/P - Índices elementos pares e ímpares do vetor (vetor).c
@@ -1,7 +1,43 @@
 #include <stdio.h>
 
+void imprimePar(int *vetor, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (vetor[i] % 2 == 0)
+        {
+            printf("%d ", i);
+        } 
+    }
+}
+
+void imprimeImpar(int *vetor, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (vetor[i] % 2 != 0)
+        {
+            printf("%d ", i);
+        } 
+    }
+}
+
 int main() {
+    int tamanho;
+
+    scanf("%d", &tamanho);
     
-    
+    int vetor[tamanho];
+
+    for (int i = 0; i < tamanho; i++)
+    {
+        int elemento;
+        scanf("%d", &elemento);
+        vetor[i] = elemento;
+    }
+    imprimePar(vetor, tamanho);
+    printf("\n");
+    imprimeImpar(vetor, tamanho);
+
     return 0;
 }
