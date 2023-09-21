4b5d3fea4257964976be656830faf45417bd4066
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-09-21 00:00:35 -0300

    Update U - Inverso (string).c

--- Lista 01 - (Revisão de APC)/U - Inverso (string).c
+++ Lista 01 - (Revisão de APC)/U - Inverso (string).c
@@ -2,6 +2,22 @@
 
 int main() {
     
+    char texto[1000] = "";
+    char caractere = 'a';
+    int i = 0;
+
+    for (; caractere != '\n'; i++)
+    {
+        scanf("%c", &caractere);
+        texto[i] = caractere;
+    }
     
+    for (int j = i - 2; j >= 0; j--)
+    //i - 1 (tamanho da str, ultimo elemento = i - 1) - 1 pq o ultimo elemento de toda string é um \n (+ um /0?)
+    {
+        printf("%c", texto[j]);
+    }
+    
+
     return 0;
 }
