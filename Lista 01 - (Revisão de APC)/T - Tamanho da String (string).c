b0d83b172efc21e9e008b3c6f5fb3aea0c69307b
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-09-21 00:00:28 -0300

    Update T - Tamanho da String (string).c

--- Lista 01 - (Revisão de APC)/T - Tamanho da String (string).c
+++ Lista 01 - (Revisão de APC)/T - Tamanho da String (string).c
@@ -2,6 +2,17 @@
 
 int main() {
     
+    char texto[1000] = "";
+    char caractere = 'a';
+    int i = 0;
+
+    for (; caractere != '\n'; i++)
+    {
+        scanf("%c", &caractere);
+        texto[i] = caractere;
+    }
     
+    printf("%d", i - 1);
+
     return 0;
 }
