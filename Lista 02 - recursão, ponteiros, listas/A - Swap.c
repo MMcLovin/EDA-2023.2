d8e51dbb75c1b954deff820fd5ec6b67eb034447
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-10-04 15:43:36 -0300

    Update A - Swap.c

--- Lista 02 - recursão, ponteiros, listas/A - Swap.c
+++ Lista 02 - recursão, ponteiros, listas/A - Swap.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
-
-int main()
+void swap(int *a, int *b)
 {
-
-
-    return 0;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
+/* int main()
+{
+    int a = 3, b = 4;
+    
+    printf("Antes da troca\na = %d\nb = %d\n", a, b);
+    
+    swap(&a, &b);
+    
+    printf("Depois da troca\na = %d\nb = %d\n", a, b);
+    
+    return 0;
+} */
