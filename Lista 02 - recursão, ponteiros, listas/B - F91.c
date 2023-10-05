3ea1fc08576ca097ec083ba8a030c6e55281eacb
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-10-05 15:51:03 -0300

    Update B - F91.c

--- Lista 02 - recursão, ponteiros, listas/B - F91.c
+++ Lista 02 - recursão, ponteiros, listas/B - F91.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 
+int f91(int N)
+{
+    if (N >= 101)
+    {
+        return (N - 10);
+    }
+    else
+    {
+        return f91(f91(N + 11));
+    }
+}
+
 int main()
 {
+    while(1)
+    {
+        int N;
 
+        scanf("%d",&N);
+
+        if (N > 0 && N <= 1000000)
+        {
+            printf("f91(%d) = %d\n", N,f91(N));
+        }
+        else if(N == 0)
+        {
+            break;
+        }
+    }
 
     return 0;
 }
