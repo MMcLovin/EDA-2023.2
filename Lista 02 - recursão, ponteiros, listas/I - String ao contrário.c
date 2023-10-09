444b5663e58082adc05d3e598703a43f7f09ed53
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-10-09 15:49:09 -0300

    Update I - String ao contrário.c

--- Lista 02 - recursão, ponteiros, listas/I - String ao contrário.c
+++ Lista 02 - recursão, ponteiros, listas/I - String ao contrário.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+
+int imprimeAoContrario(char texto[], int len)
+{
+    if (len < 0)
+    {
+        printf("\n");
+        return 0;
+    }
+    else
+    {
+        printf("%c", texto[len]);
+        return imprimeAoContrario(texto, len - 1);
+    }
+    
+}
 
 int main()
 {
+    char texto[502] = "";
+    
+    fgets(texto, sizeof(texto), stdin);
+    
+    int size = strlen(texto) - 2;
 
+    imprimeAoContrario(texto, size);
 
     return 0;
 }
