e153c3abcbb2094f4c8e87985934bae0f54a3350
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-10-09 17:41:33 -0300

    Update H - Troca X por Y.c

--- Lista 02 - recursão, ponteiros, listas/H - Troca X por Y.c
+++ Lista 02 - recursão, ponteiros, listas/H - Troca X por Y.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
+#include <string.h>
+
+int trocaX(char texto[], int i)
+{
+    if (i >= strlen(texto))
+    {
+        return 0;
+    }
+    else if (texto[i] == 'x')
+    {
+        texto[i] = 'y';
+        return trocaX(texto, i + 1);
+    }
+    else
+    {
+        return trocaX(texto, i + 1);
+    }
+    
+}
 
 int main()
 {
-
+    char texto[82] = "";
+    
+    fgets(texto, sizeof(texto), stdin);
+    
+    trocaX(texto, 0);
+    
+    fputs(texto, stdout);
 
     return 0;
 }
