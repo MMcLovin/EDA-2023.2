8bbeaef84144ea725e99ae1efbea60eb3218b820
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-10-12 16:45:35 -0300

    Update P - Inserção.c

--- Lista 02 - recursão, ponteiros, listas/P - Inserção.c
+++ Lista 02 - recursão, ponteiros, listas/P - Inserção.c
@@ -49,3 +49,81 @@ void insere_antes(celula *le, int x, int y)
     }
     insere_antes(le->prox, x, y);
 }
+/* void imprime_rec(celula *le)
+{
+    celula *node = le->prox;
+
+    if (node == NULL)
+    {
+        printf("NULL\n");
+        return;
+    }
+    else
+    {
+        printf("%d -> ", node->dado);
+        imprime_rec(le->prox);
+    }
+}
+
+//testando as funções
+celula *criacelula(int x)
+{
+    celula *newcelula;
+    newcelula = malloc(sizeof(celula));
+    newcelula->prox = NULL;
+    newcelula->dado = x;
+
+    return newcelula;
+}
+
+int main()
+{
+    
+    int input = 1;
+    while (input != 0)
+    {
+        int i = 0;
+        int n = 5;
+        
+        celula *head = criacelula(-1);
+
+        //cria uma lista com n celulas, incluindo a cabeça
+        for (; i < n; i++)
+        {
+            //scanf("%d", dado);
+            int dado = i;
+            celula *node = criacelula(dado);
+            node->prox = head->prox;
+            head->prox = node;
+        }
+
+        printf("lista original\n");
+        imprime_rec(head);
+
+        int x,y;
+
+        printf("valor 'x' a ser inserido: \n");
+        scanf("%d", &x);
+
+        printf("Valor de 'y' (x sera inserido antes de y, y<0 = insere no inicio): \n");
+        scanf("%d", &y);
+        
+        if (y >= 0)
+        {
+            printf("\ninsercao antes de y\n");
+            insere_antes(head, x, y);
+        }
+        else
+        {
+            printf("\ninsercao no inicio da lista\n");
+            insere_inicio(head, x);
+        }
+        
+        printf("lista apos a insercao\n");
+        imprime_rec(head);
+        printf("\nDeseja continuar? 0 = nao, 1 = sim\n");
+        scanf("%d", &input);
+        printf("\n");
+    }
+    return 0;
+}  */
