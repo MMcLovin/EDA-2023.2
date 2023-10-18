13358a5602fcdcf6e8fb72b023a012a734261665
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-10-18 15:42:47 -0300

    Update 02 - Desenfileira (lista encadeada).c

--- Lista 03 - TAD/02 - Desenfileira (lista encadeada).c
+++ Lista 03 - TAD/02 - Desenfileira (lista encadeada).c
@@ -6,7 +6,7 @@ typedef struct celula {
     struct celula *prox;    
 } celula;
 
-//considerando que estamos implementando uma fila com listas simplesmente encadeadas, onde as inserções são sempre realizadas ao final da lista (elementos mais novos) e as remocões no inicio da lista (elementos mais velhos)
+//considerando que estamos implementando uma fila com listas simplesmente encadeadas, onde as inserções são sempre realizadas ao final da lista (elementos mais novos sao os mais proximos da head) e as remocões no inicio da lista (elementos mais velhos sáo os mais distantes da head)
 int desenfileira(celula *fila, int *y)
 {
     if (fila->prox == NULL)
