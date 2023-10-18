f5802d54a56006d383509329250ecd8e403f739b
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-10-17 23:10:57 -0300

    Update 01 - Desenfileira (circular).c

--- Lista 03 - TAD/01 - Desenfileira (circular).c
+++ Lista 03 - TAD/01 - Desenfileira (circular).c
@@ -2,8 +2,23 @@
 #include <stdlib.h>
 #include<string.h>
 
-int main()
-{
+typedef struct {
+    int *dados;     //vetor que representa a fila
+    int N;          //tamanho do vetor
+    int p;          //inicio da fila
+    int u;          //fim da fila
+} fila;
 
-    return 0;
+int desenfileira(fila *fila, int *y)
+{
+    if (fila->p == fila->u)
+    {
+        return 0;
+    }
+    else
+    {
+        *y = fila->dados[fila->p];
+        fila->p = (fila->p + 1) % fila->N;
+        return 0;
+    }
 }
