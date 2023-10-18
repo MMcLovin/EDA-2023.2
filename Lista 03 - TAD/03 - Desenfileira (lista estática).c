74dd37a9fe1df7bd13652d632214f60a3f26ce9a
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-10-18 12:04:32 -0300

    Update 03 - Desenfileira (lista estática).c

--- Lista 03 - TAD/03 - Desenfileira (lista estática).c
+++ Lista 03 - TAD/03 - Desenfileira (lista estática).c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
 
-int main()
+typedef struct {
+    int *dados;     //vetor que representa a fila
+    int N;          //tamanho do vetor
+    int p;          //inicio da fila
+    int u;          //fim da fila
+} fila;
+
+//considerendo a implementação de uma fila com listas estáticas (vetores), cara, não é possivel que isso daqui ta certo vtnc KKKKKKK
+int desenfileira(fila *fila, int *y)
 {
+    //lista vazia
+    if (fila->p == fila->u)
+    {
+        return 0;
+    }
+    else
+    {
+        *y = fila->dados[fila->p];
+        fila->p++;
 
-    return 0;
+        return 1;
+    }
 }
