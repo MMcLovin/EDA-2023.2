946a8c579b20e367f4116852eb41a3f64573fed3
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-10-19 15:15:35 -0300

    Update 22 - Representação de pilhas em vetores.c

--- Lista 03 - TAD/22 - Representação de pilhas em vetores.c
+++ Lista 03 - TAD/22 - Representação de pilhas em vetores.c
@@ -1,9 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
 
-int main()
+typedef struct pilhaEstatica {
+    int *v;
+    int N;
+    int topo;
+} pilha;
+
+int empilha(pilha *p, int x)
 {
+    //topo invalido
+    if (p->topo > p->N || p->topo < 0)
+    {
+        return 0;
+    }
+    //pilha cheia = redimensionar v para 2*N de v
+    else if (p->topo == p->N)
+    {
+        //alocamos um espaço 2x maior q a pilha atual
+        p->N = p->N * 2;
+        int *novoV = malloc(sizeof(int) * p->N);
 
-    return 0;
+        //copia os da pilha da antiga para a nova
+        for (int i = 0; i < p->topo; i++)
+        {
+            novoV[i] = p->v[i];
+        }
+
+        p->v = novoV;
+    }
+
+    p->v[p->topo] = x;
+    p->topo++;
+
+    return 1;
+}
+
+int desempilha(pilha *p, int *y)
+{
+    //pilha vazia ou topo invalido
+    if (p->topo == 0 || p->topo > p->N || p->topo < 0)
+    {
+        //printf("Nao foi possivel desempilhar pois a pilha esta vazia :(\n");
+        return 0;
+    }
+    
+    *y = p->v[p->topo - 1];
+    p->topo--;
+
+    return 1;
 }
