a2e8ffa106b3493d56f2cf455ed5186c0af9032d
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-10-20 23:38:38 -0300

    Update 19 - Pilhas (empilha 1).c

--- Lista 03 - TAD/19 - Pilhas (empilha 1).c
+++ Lista 03 - TAD/19 - Pilhas (empilha 1).c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
 
-int main()
+typedef struct celula {
+    int dado;
+    struct celula *prox;
+} celula;
+
+//usando listas n é possivel ter uma lista cheia
+//implementar uma pilha usando listas encadeadas, é nada mais nada menos do que usar listas simplesmente encadeadas, sempre realizando tanto as inserções quanto remoções na cabeça da lista 
+int empilha(celula *p, int x)
 {
+    celula *node = malloc(sizeof(celula));
+    node->prox = p->prox;
+    p->prox = node;
+    node->dado = x;
 
-    return 0;
+    return 1;
 }
