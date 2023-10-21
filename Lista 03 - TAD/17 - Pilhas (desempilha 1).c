a8821d85b71549ec5bf63c34ed206ffe8f9a4b29
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-10-20 23:34:05 -0300

    Update 17 - Pilhas (desempilha 1).c

--- Lista 03 - TAD/17 - Pilhas (desempilha 1).c
+++ Lista 03 - TAD/17 - Pilhas (desempilha 1).c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
 
-int main()
+typedef struct celula {
+    int dado;
+    struct celula *prox;
+} celula;
+
+//KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK
+//eh so isso msm
+int desempilha(celula *p, int *y)
 {
+    if(p->prox == NULL)
+    {
+        return 0;
+    }
+    
+    *y = p->prox->dado;
+    p->prox = p->prox->prox;
 
-    return 0;
+    return 1;
 }
