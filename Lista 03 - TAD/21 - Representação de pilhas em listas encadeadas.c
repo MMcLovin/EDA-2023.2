89dd6fc1b57663910d88de7991c1284396dd84cd
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-10-24 00:41:54 -0300

    Update 21 - Representação de pilhas em listas encadeadas.c

--- Lista 03 - TAD/21 - Representação de pilhas em listas encadeadas.c
+++ Lista 03 - TAD/21 - Representação de pilhas em listas encadeadas.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
 
-int main()
+typedef struct celula {
+    int dado;
+    struct celula *prox;
+} celula;
+
+int desempilha(celula *p, int *y)
 {
+    if(p->prox == NULL)
+    {
+        return 0;
+    }
+    
+    *y = p->prox->dado;
+    p->prox = p->prox->prox;
 
-    return 0;
+    return 1;
+}
+
+int empilha(celula *p, int x)
+{
+    celula *node = malloc(sizeof(celula));
+    node->prox = p->prox;
+    p->prox = node;
+    node->dado = x;
+
+    return 1;
 }
