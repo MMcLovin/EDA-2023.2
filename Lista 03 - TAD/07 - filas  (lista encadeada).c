fe76f8f1c00df69d2655a0ab55623528eef16266
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-10-24 00:32:50 -0300

    Update 07 - filas  (lista encadeada).c
    I still cant fully grasp the reasonning behind the enfileira function

--- Lista 03 - TAD/07 - filas  (lista encadeada).c
+++ Lista 03 - TAD/07 - filas  (lista encadeada).c
@@ -1,9 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
 
+//lista encadeada circular
+typedef struct celula {
+    int dado;
+    struct celula *prox;
+} celula;
+
+void enfileira(celula **fila, int x)
+{
+    celula *newHead = malloc(sizeof(celula));
+    
+    //se a fila estiver vazia: fila->prox == fila 
+    newHead->prox = (*fila)->prox;
+    (*fila)->prox = newHead;
+    (*fila)->dado = x;
+
+    *fila = newHead;
+}
+
+int desenfileira(celula *fila, int *y)
+{
+    if (fila->prox == fila)
+    {
+        return 0;
+    }
+    
+    *y = fila->prox->dado;
+    fila->prox = fila->prox->prox;
+
+    return 1;
+}
+
+/*
 int main()
 {
+    celula **f = malloc(sizeof(celula));
+    enfileira(f, 69);
+    printf("%d\n", (*f)->prox->dado);
 
     return 0;
 }
+*/
