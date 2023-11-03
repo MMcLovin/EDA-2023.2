6f10e34e004e12fd3534c1807352b99661d187d4
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-11-03 13:17:12 -0300

    Update 23 - Árvores - percurso inOrder.c

--- Lista 03 - TAD/23 - Árvores - percurso inOrder.c
+++ Lista 03 - TAD/23 - Árvores - percurso inOrder.c
@@ -1,9 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
 
-int main()
+typedef struct no {
+    int dado;
+    struct no *esq, *dir;
+} no;
+
+typedef struct celula {
+    struct no *no;
+    struct celula *prox;
+} celula;
+
+int isPilhaVazia(celula *p)
 {
+    if (p->prox == NULL)
+    {
+        return 1;
+    }
 
     return 0;
+}
+
+no *desempilha(celula *p)
+{
+    if (isPilhaVazia(p)) return 0;
+
+    no *noh = p->prox->no;
+    p->prox = p->prox->prox;
+
+    return noh;
+}
+
+int empilha(celula *p, no *x)
+{   
+    celula *new = malloc(sizeof(celula));
+    new->no = x;
+    new->prox = p->prox;
+    p->prox = new;
+
+    return 1;
+}
+
+void em_ordem (no *raiz)
+{
+    celula *pilha = malloc(sizeof(celula));
+    pilha->prox = NULL;
+    no *x = raiz;
+
+    while (1)
+    {
+        while (x != NULL)
+        {
+            empilha(pilha, x);
+            x = x->esq;
+        }
+        
+        if (isPilhaVazia(pilha)) break;
+        
+        x = desempilha(pilha);
+        printf("%d ", x->dado);
+        x = x->dir;
+    }
+
+    printf("\n");
 }
