002dfb47523071131bdd29e9d75df98c7e3d248d
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-11-03 14:32:13 -0300

    Update 25 - Árvores - percurso preOrder.c

--- Lista 03 - TAD/25 - Árvores - percurso preOrder.c
+++ Lista 03 - TAD/25 - Árvores - percurso preOrder.c
@@ -1,9 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
 
-int main()
+typedef struct no {
+    int dado;
+    struct no *esq, *dir;
+} no;
+
+typedef struct celula {
+    struct no *noh;
+    struct celula *prox;
+} celula;
+
+int isPilhaVazia(celula *p)
 {
+    if (p->prox == NULL)
+    {
+        return 1;
+    }
 
     return 0;
+}
+
+no *desempilha(celula *p)
+{
+    if (isPilhaVazia(p)) return 0;
+
+    no *noh = p->prox->noh;
+    p->prox = p->prox->prox;
+
+    return noh;
+}
+
+int empilha(celula *p, no *x)
+{   
+    celula *new = malloc(sizeof(celula));
+    new->noh = x;
+    new->prox = p->prox;
+    p->prox = new;
+
+    return 1;
+}
+
+void pre_ordem (no *raiz)
+{
+    if (raiz == NULL) return;
+
+    celula *pilha = malloc(sizeof(celula));
+    pilha->prox = NULL;
+    no *x = raiz;
+
+    empilha(pilha, raiz);
+
+    while (!isPilhaVazia(pilha))
+    {
+        raiz = desempilha(pilha);
+
+        printf("%d ", raiz->dado);
+
+        if (raiz->dir != NULL)
+        {
+            empilha(pilha, raiz->dir);
+        }
+
+        if (raiz->esq != NULL)
+        {
+            empilha(pilha, raiz->esq);
+        }
+    }
+    printf("\n");
 }
