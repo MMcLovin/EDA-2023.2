648294e66b0d09b251f54fce38673378c8b24e1d
MMcLovin <122990047+MMcLovin@users.noreply.github.com>
2023-11-12 17:01:16 -0300

    Update 08 - filas  (lista estática).c

--- Lista 03 - TAD/08 - filas  (lista estática).c
+++ Lista 03 - TAD/08 - filas  (lista estática).c
@@ -1,9 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include<string.h>
+#include<stdio.h>
 
-int main()
-{
+typedef struct {
+    int *v;       //vetor que representa a fila
+    int N;        //tamanho do vetor
+    int inicio;   //inicio da fila
+    int fim;      //fim da fila
+} fila;
 
-    return 0;
+int enfileira (fila *f, int x){
+  if((f->fim+1) % f->N == f->inicio) return 0;
+  
+  f->v[f->fim] = x;
+  f->fim = (f->fim+1) % f->N;
+
+  if(f->inicio == f->fim) (f->inicio+1) % f->N;
+
+  return 1;
+}
+
+int desenfileira (fila *f, int *y){
+  if(f->inicio==f->fim) return 0;
+  
+  *y=f->v[f->inicio];
+  
+  f->inicio = (f->inicio+1) % f->N;
+  return 1;
 }
